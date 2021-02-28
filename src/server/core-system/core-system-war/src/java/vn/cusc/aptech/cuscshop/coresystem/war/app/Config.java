91628e17e9d6ebe471c1fd0ea637c41016f22e65
Daomtthuan <dao.mt.thuan@gmail.com>
2021-02-28 21:45:27 +0700

    setup theme

--- src/server/core-system/core-system-war/src/java/vn/cusc/aptech/cuscshop/coresystem/war/app/Config.java
+++ src/server/core-system/core-system-war/src/java/vn/cusc/aptech/cuscshop/coresystem/war/app/Config.java
@@ -11,7 +11,6 @@ public class Config implements Serializable {
 
     private final AppConfig app;
     private final DirectoryConfig directory;
-    private final VendorsConfig vendors;
 
     public Config() {
         this.app = new AppConfig(
@@ -25,15 +24,6 @@ public class Config implements Serializable {
                 "/WEB-INF/layouts", // layouts
                 "/WEB-INF/components" // components
         );
-
-        this.vendors = new VendorsConfig(
-                new String[]{
-                    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
-                }, // styles
-                new String[]{
-                    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
-                } // scripts
-        );
     }
 
     public AppConfig getApp() {
@@ -42,10 +32,6 @@ public class Config implements Serializable {
 
     public DirectoryConfig getDirectory() {
         return directory;
-    }
-
-    public VendorsConfig getVendors() {
-        return vendors;
     }
 
 }
