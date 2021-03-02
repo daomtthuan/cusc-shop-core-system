4fec4efa0642e7c7143b883da55366b7c6a38dc0
Daomtthuan <dao.mt.thuan@gmail.com>
2021-03-02 12:57:49 +0700

    change theme

--- src/server/core-system/core-system-war/src/java/vn/cusc/aptech/cuscshop/coresystem/war/app/config/DirectoryConfig.java
+++ src/server/core-system/core-system-war/src/java/vn/cusc/aptech/cuscshop/coresystem/war/app/config/DirectoryConfig.java
@@ -1,13 +1,17 @@
 package vn.cusc.aptech.cuscshop.coresystem.war.app.config;
 
+import javax.faces.context.FacesContext;
+
 public class DirectoryConfig {
 
     private final String layouts;
     private final String components;
+    private final String root;
 
     public DirectoryConfig(String layouts, String components) {
         this.layouts = layouts;
         this.components = components;
+        this.root = FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
     }
 
     public String getLayouts() {
@@ -17,4 +21,9 @@ public class DirectoryConfig {
     public String getComponents() {
         return components;
     }
+
+    public String getRoot() {
+        return root;
+    }
+
 }
